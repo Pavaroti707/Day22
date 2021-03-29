76a8fea4fb60204bc084869c857202c100d774f6
Pavaroti707 <hasicmahir@gmail.com>
2021-03-29 22:01:57 +0200

    ex08

---
+++ ex08/src/Table.js
@@ -0,0 +1,42 @@
+// src/Table.js
+
+import React, { Component } from "react";
+
+// Add TableHeader arrow function here
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>First name</th>
+        <th>Last name</th>
+      </tr>
+    </thead>
+  );
+};
+
+// Add TableBody arrow function here
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.firstName}</td>
+        <td>{row.lastName}</td>
+      </tr>
+    );
+  });
+  return <tbody>{rows}</tbody>;
+};
+
+class Table extends Component {
+  render() {
+    const { characterData } = this.props; // Add this line
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} />
+      </table>
+    );
+  }
+}
+
+export default Table;
