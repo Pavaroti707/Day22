95455f737c10ddd8a36daee164266ea2309ce3cb
Pavaroti707 <hasicmahir@gmail.com>
2021-03-29 22:21:21 +0200

    ex10

---
+++ ex10/src/Box.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Box = (props) => {
+  // only change code below this line
+  const { size } = props;
+  return <h2>This is a {size}px Component!</h2>;
+  // only change code below this line
+};
+
+export default Box;
